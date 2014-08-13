3315f90be2edea36ac3a3cf45cebf724703b5873
jennifermccarthy <jennifernward@gmail.com>
2014-08-13 02:54:48 +0000

    will save all instances of a Book class

--- lib/books.rb
+++ lib/books.rb
@@ -4,7 +4,25 @@ class Book
 
   def initialize(attributes) 
     @title = attributes['title']
-    @id = attributes['id']
+    @id = attributes['id'].to_i
+  end
+  
+  def self.all 
+    books = []
+    results = DB.exec("SELECT * FROM books;")
+    results.each do |result|
+      books << Book.new(result)
+    end  
+    books
+  end
+  
+  def save
+    results = DB.exec("INSERT INTO books (title) VALUES ('#{@title}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+  
+  def ==(another_book)
+    @title == another_book.title && @id == another_book.id
   end
  
 end
--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -1,7 +1,16 @@
 require 'rspec'
-require 'books.rb'
+require 'books'
+require 'pg'
+
+DB = PG.connect({:dbname => 'library_test'})
 
 describe Book do
+  
+  RSpec.configure do |config|
+    config.after(:each) do
+      DB.exec("DELETE FROM books *;")
+    end
+  end
   
   it 'will initialize a book instance with a title' do
     test_book = Book.new({'title' => 'Gone With the Wind'})
@@ -11,6 +20,12 @@ describe Book do
   it 'will read out the title of a book' do
     test_book = Book.new({'title' => 'Gone With the Wind'})
     expect(test_book.title).to eq 'Gone With the Wind'
+  end
+  
+  it 'will save all instances of Book class' do
+    test_book = Book.new({'title' => 'Gone With the Wind'})
+    test_book.save
+    expect(Book.all).to eq [test_book]
   end
 
 end
